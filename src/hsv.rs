f43b12b00869d7474240bbaf2fa7482377574111
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-12-14 20:32:22 +0100

    Update for Rustc

--- src/hsv.rs
+++ src/hsv.rs
@@ -23,7 +23,7 @@ fn cast<T: num::NumCast, U: num::NumCast>(n: T) -> U {
     num::cast(n).unwrap()
 }
 
-#[deriving(Clone, PartialEq, Eq, Show)]
+#[deriving(Clone, Copy, PartialEq, Eq, Show)]
 pub struct Hsv<T> { pub h: T, pub s: T, pub v: T }
 
 impl<T: FloatChannel> Hsv<T> {
